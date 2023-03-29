40ae48fef80ced556fa20b5f939c0f12b09e64c5
Mateusz Ciszczoń <contact@mciszczon.pl>
2023-03-29 12:43:02 +0200

    Add plop generator for components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
+/* eslint-disable sort-imports, import/order */
+
+/* PLOP_INJECT_EXPORT */
 export { default as Footer } from './Footer'
 export { default as Features } from './Features'
 export { default as Hero } from './Hero'
