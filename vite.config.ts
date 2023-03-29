f5b9a7eb484ba09dfa147df40b5e1f3813533235
Mateusz Ciszczoń <contact@mciszczon.pl>
2023-03-29 14:41:15 +0200

    Fix vite config base url

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://instytutfi.github.io/frontend-starter/',
   build: {
     outDir: 'dist'
   },
